55411eec63d09901d760f59adfdf318c9f1bfc37
spankr <spankrrules@gmail.com>
2016-10-19 10:29:22 +0530

    move any add file to backup folder, setup file logging

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
+var fs = require('fs-extra');
 var path = require('path');
 var argv = require('yargs').argv;
 var chokidar = require('chokidar');
-var log = console.log.bind(console);
+var winston  = require('winston');
 
-var folderToWatch = '.';
-folderToWatch = (argv.length > 1)? path.resolve(argv._[0]) : path.resolve(folderToWatch);
-
-console.log('Watching folder: ', folderToWatch);
-var watcher = chokidar.watch(folderToWatch, {
-  ignored: [/[\/\\]\./],
-  ignoreInitial: true,
-  awaitWriteFinish: true,
-  depth: 0
+// setup logger
+winston.add(require('winston-daily-rotate-file'), {
+    filename: './logs/app.log',
+    zippedArchive: true
 });
 
+
+// setup chowkidar watcher
+var backupFolder  = path.resolve('./backup');
+var folderToWatch = '.';
+folderToWatch = (argv.length > 1)? path.resolve(argv._[0]) : path.resolve(folderToWatch);
+var watcher = chokidar.watch(folderToWatch, {
+    ignored: [/[\/\\]\./],
+    ignoreInitial: true,
+    awaitWriteFinish: true,
+    depth: 0
+});
+
+
+// start watching for files
+winston.info('Watching folder: ', folderToWatch);
 watcher.on('add', fileAdded);
 
-//////////
 
-function fileAdded(path) {
-  log(`File ${path} has been added`);
+////////////////////
+function fileAdded(file, event) {
+    winston.info(`File ${file} has been added`);
+    moveFile(file);
+}
+
+function moveFile(file) {
+    var name = path.basename(file);
+    var moveHere = backupFolder +'/'+ name;
+    fs.move(file, moveHere, { clobber: true }, function (err) {
+        if (err) return winston.error(err);
+        winston.info(`File moved to ${moveHere}`);
+    });
 }
